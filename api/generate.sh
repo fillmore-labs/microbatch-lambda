f9f2df3ad046628230208c1c97c56760091619d9
Oliver Eikemeier <eikemeier@fillmore-labs.com>
2024-01-11 12:33:08 +0100

    Change API from JSON to Protocol Buffers
    Signed-off-by: Oliver Eikemeier <eikemeier@fillmore-labs.com>

---
+++ api/generate.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -eu
+
+if ! command -v protoc-gen-go &> /dev/null; then
+  go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.32.0
+fi
+
+for proto_path in v1alpha1; do
+
+  protoc \
+    --go_opt=module=github.com/fillmore-labs/microbatch-lambda/api \
+    --go_opt=paths=import \
+    --go_out=. \
+    proto/$proto_path/*.proto
+
+done
--- api/go.mod
+++ api/go.mod
@@ -3,3 +3,5 @@ module github.com/fillmore-labs/microbatch-lambda/api
 go 1.21
 
 toolchain go1.21.5
+
+require google.golang.org/protobuf v1.32.0
